b0d53f9b4c754c25ba2812c45264aaf7371e3f13
Yossi Eikelman <yussiroz@gmail.com>
2022-01-16 16:05:33 +0200

    Initial model setup <Patient, Record>

---
+++ client_app/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Client(models.Model):
+    f_name = models.CharField(max_length=200)
+    l_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+
+    description = models.TextField()
+
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Client, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.f_name} {self.l_name}, {self.age}"
+
+
+class Record(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='record_patient')
+
+    description = models.TextField()
+
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Record, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.id}, {self.client.f_name} {self.client.l_name}"
+
